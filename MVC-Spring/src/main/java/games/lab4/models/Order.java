6a16bb94edad77a316b9e04309de6638eda714e8
KrycuX <59706218+KrycuX@users.noreply.github.com>
2021-01-26 16:25:32 +0100

    notatki

--- MVC-Spring/src/main/java/games/lab4/models/Order.java
+++ MVC-Spring/src/main/java/games/lab4/models/Order.java
@@ -3,25 +3,20 @@ package games.lab4.models;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Order {
-    @Id
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private Date orderDate;
-    private int orderNum;
-    private double amount;
     private float price;
 
-    @ManyToOne
-    private Game game;
+
     @ManyToOne
     private User user;
 }
